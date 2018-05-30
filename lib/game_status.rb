45d081ef9467e90b1d70264f88796e7bab588311
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-30 19:30:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == WIN_COMBINATIONS || draw?(board) == true || full?(board) == true
+  if won?(board) || draw?(board) || full?(board)
     return true
   end
 end
